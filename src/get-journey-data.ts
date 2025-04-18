a4b31cf77e3da7ea65f55648e8a1df94f854818b
Bruno Leandro Da Silva <bdasilva@intellicheck.com>
2025-04-18 16:04:52 -0500

    added user journey data

---
+++ src/get-journey-data.ts
@@ -0,0 +1,80 @@
+import axios, { AxiosResponse } from "axios";
+import crypto from "crypto";
+import { saveResponseToFile } from "./utils/saveResponseToFile";
+import { ENV, EN_US } from "./utils/constants";
+import { JourneyDataPayload } from "./types/journeyDataPayload";
+
+async function getJourneyData(): Promise<void> {
+  try {
+    const {
+      CUSTOMER_ID,
+      API_KEY,
+      BASE_URL,
+      END_POINT_GET_RESULTS,
+      TRANSACTION_ID_TWO,
+    } = ENV;
+
+    if (
+      !CUSTOMER_ID ||
+      !API_KEY ||
+      !BASE_URL ||
+      !END_POINT_GET_RESULTS ||
+      !TRANSACTION_ID_TWO
+    ) {
+      console.error("Log Error: ❌ Missing environment variables.");
+      process.exit(1);
+    }
+
+    const YOUR_TRANSACTION_ID = TRANSACTION_ID_TWO || "YOUR_TRANSACTION_ID";
+
+    const payload: JourneyDataPayload = {
+      public_data: {
+        capture_language: EN_US,
+      },
+      private_data: {
+        transaction_id: YOUR_TRANSACTION_ID,
+        include_pii: true,
+        start_date: "2025-03-20",
+        end_date: "2023-04-17",
+      },
+    };
+
+    const payloadString: string = JSON.stringify(payload);
+    const base64EncodedPayload: string =
+      Buffer.from(payloadString).toString("base64");
+
+    const hmac = crypto.createHmac("sha256", API_KEY);
+    hmac.update(base64EncodedPayload);
+
+    const signature: string = "sha256=" + hmac.digest("hex");
+
+    const headers: Record<string, string> = {
+      "customer-id": CUSTOMER_ID,
+      signature: signature,
+      Accept: "application/json",
+    };
+
+    const response: AxiosResponse<string> = await axios.post(
+      BASE_URL + END_POINT_GET_RESULTS,
+      base64EncodedPayload,
+      { headers }
+    );
+
+    const responseSignatureLog: string | undefined =
+      response.headers["signature"];
+    console.log("Log Info: ✍️ Response Signature:", responseSignatureLog);
+
+    const responseBody: string = Buffer.from(response.data, "base64").toString(
+      "utf8"
+    );
+    console.log("Log Info: 📄 Response Body:", responseBody);
+
+    console.log(" Log Info: 💾 Save response to file");
+
+    await saveResponseToFile(responseBody, "RESULTS");
+  } catch (error: any) {
+    console.error("Log Error: ❌", error.message);
+  }
+}
+
+getJourneyData();
