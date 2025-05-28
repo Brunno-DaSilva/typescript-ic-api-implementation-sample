61c251d32022487a74dfecd5218be5a6c8f57345
Bruno Da Silva <hi.dasilvab@gmail.com>
2025-05-28 08:59:45 -0500

    Configured start payload  type, added public data urls

--- src/types/startPayload.ts
+++ src/types/startPayload.ts
@@ -9,21 +9,24 @@ interface PrivateData {
   document_type?: string;
   location?: string;
   purpose?: string;
-  return_capture_url?: boolean;
   return_images?: boolean;
   send_link?: SendLink;
   signals?: string[];
   ttl?: number; // between 10–30
   user_defined?: Record<string, unknown>;
+  return_capture_url?: boolean;
+}
+
+interface PublicData {
+  qr_breakpoint_px?: number;
+  redirect_url?: string;
+  error_redirect_url?: string;
+  results_url?: string;
+  status_url?: string;
+  capture_language?: string;
 }
 
 export interface StartPayload {
-  public_data: {
-    capture_language?: string;
-    redirect_url?: string;
-    error_redirect_url?: string;
-    results_url?: string;
-    status_url?: string;
-  };
+  public_data: PublicData;
   private_data: PrivateData;
 }
